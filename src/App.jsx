aa201b7d22dfa570ee13429e875c53597ac7fe2f
ejpasayama-bit <ejp.asayama@gmail.com>
2026-08-15 19:21:51 +0900

    Initial commit: EasyJ Studio Website with i18n

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { LanguageProvider } from './LanguageContext';
 import Header from './components/Header';
 import Footer from './components/Footer';
-import Home from './pages/Home';
-import Concept from './pages/Concept';
-import Profile from './pages/Profile';
-import Contact from './pages/Contact';
-import Guidelines from './pages/Guidelines';
 import Local from './pages/Local';
-import ListeningAdventure from './pages/ListeningAdventure';
-import KickstarterGuide from './pages/KickstarterGuide';
 
 function App() {
   return (
@@ -20,14 +13,8 @@ function App() {
         <Header />
         <main>
           <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/concept" element={<Concept />} />
-            <Route path="/profile" element={<Profile />} />
-            <Route path="/contact" element={<Contact />} />
-            <Route path="/guidelines" element={<Guidelines />} />
             <Route path="/local" element={<Local />} />
-            <Route path="/listening-adventure" element={<ListeningAdventure />} />
-            <Route path="/ks-guide" element={<KickstarterGuide />} />
+
           </Routes>
         </main>
         <Footer />
